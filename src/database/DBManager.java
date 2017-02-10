6eee3e125e95db0fe89d5b46f8d72078e5f2a87e
gautam5sept@gmail.com <gautam5sept@gmail.com>
2017-02-11 00:21:00 +0530

    PiChart Functionality Added

--- src/database/DBManager.java
+++ src/database/DBManager.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.google.gson.Gson;
 import commonLib.CommonLogs;
 
 public class DBManager {
@@ -12,21 +15,23 @@ public class DBManager {
 	private static String DBUrl = "jdbc:mysql://localhost:3306/faterun";
 	private static String DBCon = "com.mysql.jdbc.Driver";
 	private static String DbUserName = "root";
-	private static String DbUserPassword = "";
+	private static String DbUserPassword = "admin";
 
 	static {
 		try {
 			Class.forName(DBCon);
+			CommonLogs.writeServerLogFile("Connected to the database successfully");
 		} catch (Exception xa) {
-			CommonLogs.writeServerLogFile("Unable to Load JDBC Driver ");
+			CommonLogs.writeServerLogFile("Unable to Load JDBC Driver "+xa);
 		}
 	}
 
-	public static boolean getPiChartData() {
-		boolean flag = false;
+	public static String getPiChartData() {
+		Map <String,Integer> mp=new LinkedHashMap<String,Integer>();
+		String json=null;
 		int totalCount=0;
 		int fail=0;
-		int pass=0;
+		int total=0;
 		Date d=new Date();
 		SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MM-yyy hh:mm:ss");
 		try {
@@ -39,8 +44,9 @@ public class DBManager {
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()){
 				totalCount=rs.getInt(1);
+				mp.put("totalRun", totalCount);
 			}else{
-				CommonLogs.writeServerLogFile("No field selected for total no of test cases from regrun "
+				CommonLogs.writeServerLogFile("No field selected for total no of test cases from faterun.regrun "
 						+ "table"+dateFormat.format(d.getDate()));
 			}
 
@@ -51,17 +57,29 @@ public class DBManager {
 			rs=ps.executeQuery();
 			if(rs.next()){
 				fail=rs.getInt(1); 
+				mp.put("failures", fail);
 			}else{
-				CommonLogs.writeServerLogFile("No field selected for total  no of failures from regrun "
+				CommonLogs.writeServerLogFile("No field selected for total  no of failures from faterun.regrun. "
 						+ "table"+dateFormat.format(d.getDate()));
 			}
 
-			//To get total number of passed test case from last run
-			pass=totalCount-fail; 
+			//calculating total number of testcase exists in the system
+			ps=con.prepareStatement("select testcase from faterun.testcount where Mtype='total'");
+			rs=ps.executeQuery();
+			if(rs.next()){
+				total=rs.getInt(1);
+				mp.put("total", total);
+			}else{
+				CommonLogs.writeServerLogFile("No field selected for total  no of testcase from faterun.totalCount "
+						+ "table"+dateFormat.format(d.getDate()));	
+			}
+			con.close();
+			//Creating Json object to send back to Ajax call
+			json=new Gson().toJson(mp);
 
 		} catch (Exception xa) {
 			CommonLogs.writeServerLogFile("Can't find Require Fields from Database :: "+dateFormat.format(d.getDate()));
 		}
-		return flag;
+		return json;
 	}
 }
---
+++ src/services/GetCurrentTestCases.java
@@ -0,0 +1,48 @@
+package services;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.DBManager;
+
+/**
+ * Servlet implementation class GetCurrentTestCases
+ */
+@WebServlet("/GetCurrentTestCases")
+public class GetCurrentTestCases extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCurrentTestCases() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String json=DBManager.getPiChartData();
+		System.out.println(json);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		doGet(request, response);
+	}
+
+}
